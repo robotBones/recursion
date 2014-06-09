8748d29f076500c2a792bee22d0b69be2205810f
Kim Marquardt <kim.marquardt@gmail.com>
2014-06-09 00:23:28 -0700

    Takes Objects and returns string
    *contains console.logging and test objects.
    * return valueis a string that slightly resembles expected JSON output.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,11 +2,63 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+function howNested(n) {
+  var thingy = "*";
+  var result = "";
+  for ( var i = 0; i < n; i++) {
+      result += thingy;
+  }
+  return result;
+}
+var x = {
+  a: 1,
+  2: "two",
+  c: [],
+  d: {2: [3,4]},
+  e: "all above"
+}
+console.log();
+console.log();
+console.log("//////////////////////////////////////////////////////////////////////");
+var layer = 0;
 var stringifyJSON = function(obj) {
   // your code goes here
-  _.each(obj, function (val, key, o) {
-    console.log(key);
-    console.log(val);
-    console.log(o);
-  });
+  var JSON = '';
+  layer += 1
+
+  for (key in obj) {
+    console.log(howNested(layer) + Object.prototype.toString.call(obj[key]) + " - " + key + ":",obj[key]);
+    var type = Object.prototype.toString.call(obj[key]);
+    if (type !== '[object Object]' && type !== '[object Array]') {
+      JSON += key + ":" + obj[key] + ",";
+    }
+    else if (type === '[object Object]') {
+      JSON += key + ":" + stringifyJSON(obj[key]);
+    } else if (type === '[object Array]') {
+      var ar =
+      JSON += [key,":","[", stringifyJSON(obj[key]), "]"].join("");
+    }
+  }
+
+
+  layer -= 1;
+  if (layer === 0) {
+    JSON = JSON.slice(0,-1);
+    return ['{',JSON,'}'].join("")
+  } else {
+    return JSON;
+  }
+
 };
+
+
+var output = stringifyJSON(x);
+console.log("//////////////////////////////////////////////////////////////////////");
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log("object: ",x);
+console.log("JSON: "+JSON.stringify(x));
+console.log("myJunk: ", output);
