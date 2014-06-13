0fedd8eb80b5b79a6ce165b6c5fac7a6ccbd514d
Kim Marquardt <kim.marquardt@gmail.com>
2014-06-12 18:46:28 -0700

    getElementsByClassName passes specs via recursive call of an inner function.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,31 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
+var getElementsByClassName = function(className) {
   // your code here
   var els = [];
-  console.log();
-  console.log();
-  console.log("inside new fn context");
-  console.log('is node defined: ', node);
-  var node = node || document.body;
-  var nodes = node.childNodes;
-  var element;
-  console.log('node for this context is: ', node);
-  console.log('childNodes: ', nodes);
-  for (var i = 0; i < 4; i++) {
-    console.log('in loop');
-    element = nodes[i];
-    console.log('classlist: ',nodes[0].classList);
-    // check if element is an element node. Only those have classList property
-    if ( element.nodeType === 1 && element.classList.contains(className) ) {
-      console.log(element);
-      console.log('passed conditions');
-      console.log(element.tagName, 'has class' );
-      els.push(element);
+  var node = document.body;
+
+  var recurser = function(node) {
+    var children = node.childNodes;
+
+    if ( node.nodeType === 1 && node.classList.contains(className) ) {
+      els.push(node);
     }
-    console.log('should be looping', i);
+
+    for (var i = 0; i < children.length; i++) {
+      recurser(children[i]);
+    }
   }
-  console.log('about to return: ', els);
+
+  recurser(node);
+
   return els;
 };
